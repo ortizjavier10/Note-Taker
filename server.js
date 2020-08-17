73f6f889370188f6e07176e3e98325cec26be862
JAVI <ortiz.javier10@gmail.com>
2020-08-16 20:50:42 -0400

    server.js file app.use

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('./routes/apiRoute');
+
+const htmlRoutes = require('./routes/htmlRoute');
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static('public'));
+
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+app.use('/', htmlRoutes);
